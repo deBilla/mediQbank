c217c868d932a50d4e04afe04d13af1571603972
w_dimuthu <w.dimuthu@directfn.com>
2020-10-24 03:53:55 +0530

    User controller created 2

---
+++ src/main/java/com/debilla/mediqbank/user/UserController.java
@@ -0,0 +1,34 @@
+package com.debilla.mediqbank.user;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@PostMapping(path="/add") // Map ONLY POST Requests
+	public @ResponseBody String addNewUser (@RequestParam String name
+		, @RequestParam String email) {
+		// @ResponseBody means the returned String is the response, not a view name
+		// @RequestParam means it is a parameter from the GET or POST request
+
+		User n = new User();
+		n.setName(name);
+		n.setEmail(email);
+		userRepository.save(n);
+		return "Saved";
+	}
+
+	@GetMapping(path="/all")
+	public @ResponseBody Iterable<User> getAllUsers() {
+		// This returns a JSON or XML with the users
+		return userRepository.findAll();
+	}
+}
