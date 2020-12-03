a402943bc7f24048d9d89f48417c8af6b1c0a219
w_dimuthu <w.dimuthu@directfn.com>
2020-12-04 00:01:33 +0530

    Laptop cleaning

---
+++ src/main/java/com/debilla/mediqbank/fileController/FileController.java
@@ -0,0 +1,47 @@
+package com.debilla.mediqbank.fileController;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class FileController {
+    @PostMapping(path = "/upload",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+
+    public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+        File convertFile = new File("/var/tmp/"+file.getOriginalFilename());
+        convertFile.createNewFile();
+        FileOutputStream fout = new FileOutputStream(convertFile);
+        fout.write(file.getBytes());
+        fout.close();
+        return "File is upload successfully";
+    }
+
+    @GetMapping(path = "/download")
+    public ResponseEntity<Object> downloadFile() throws IOException  {
+        String filename = "/var/tmp/mysql.png";
+        File file = new File(filename);
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getName()));
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        ResponseEntity<Object>
+                responseEntity = ResponseEntity.ok().headers(headers).contentLength(file.length()).contentType(
+                MediaType.parseMediaType("application/txt")).body(resource);
+
+        return responseEntity;
+    }
+}
